d53d44a098b8fcde32cf3c897d1dd60a3fb2c426
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2023-08-05 23:04:12 -0300

    mudança de idioma add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-let showOpen = document.querySelector('#open')
 let showOpen2 = document.querySelector('.open')
 
 function show() {
   showOpen2.classList.toggle('active')
-
 }
 
+let flagOpen = document.querySelector(".flag_open")
+
+function flagShow () {
+    flagOpen.classList.toggle('showFlag')
+}
+
+const translate = {
+  pt: {
+    home: "Inicio", 
+    learn: "Lendo",
+    more: "Mais"
+  }, 
+  ja: {
+    home: "ホーム", 
+    learn: "ラーニング",
+    more: "モア"
+  },
+  en: {
+    home: "Home", 
+    learn: "Learning",
+    more: "More"
+  }
+}
+
+function changeLanguage(language) {
+  const elements = document.querySelectorAll('[data-translate]')
+
+  for (const element of elements) {
+    const key = element.getAttribute('data-translate')
+    element.textContent = translate[language][key]
+  }
+}
+
+document.getElementById('ptLang').addEventListener('click', () => changeLanguage('pt'));
+document.getElementById('jaLang').addEventListener('click', () => changeLanguage('ja'));
+document.getElementById('usaLang').addEventListener('click', () => changeLanguage('en'));
